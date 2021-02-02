f501519ce5554aa57c87f279bbac13bee6ca0531
Filip3077 <77790109+Filip3077@users.noreply.github.com>
2021-02-02 18:29:37 +0100

    Delete HyperSpy_testSpectra.py

--- HyperSpy_testSpectra.py
+++
@@ -1,7 +0,0 @@
-#Test av att plotta spektra med HyperSpy, det spektrat som användes var genererat från DSTA-II mha deras exempelscript. 
-import matplotlib as mpl
-import hyperspy.api as hs
-
-
-s = hs.load("testspectraAu.msa",reader="msa",signal_type = "EDS_TEM")
-s.plot()
