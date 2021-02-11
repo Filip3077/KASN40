ba27651d85ebeffbb25708da9186a268a4d8e322
LucNVIII <77793588+LucNVIII@users.noreply.github.com>
2021-02-11 18:10:12 +0100

    Börjar bli nått
    Fortfarande inte så mycket nytt men behövde få testa runt lite. Hade inte lika mycket tid idag som jag tänkte så kom inte så långt men om ni vill kan ni leka runt i untitled0.py och se hur den separerar beroende på hur många faktorer man väljer, vilka spektrum man väljer samt, etc. Det verkar som den kan få ganska bra separation med både NMF och BSS men det beror lite på spektrum och antal komponenter tror jag.

--- Lucas v.6/untitled0.py
+++ Lucas v.6/untitled0.py
@@ -4,7 +4,8 @@ Created on Thu Feb 11 08:48:56 2021
 
 @author: Lucas
 """
-%matplotlib qt
+
+import matplotlib.pyplot as plt
 import hyperspy.api as hs
 import numpy as np
 from coreshellp import CoreShellP, CoreShellSpec
@@ -14,8 +15,8 @@ from edxmat import EdxMat
 dens = 1 #Eftersom vi 1**-3 = 1 så är det bara "teoretiskt" nm^-3 ändå...
 x = CoreShellP(50,20.0,15.0,dens,dens,1) #50x50 pixlar,r = 20nm (d=40),core r = 15nm, "densities=1nm^-3", pixel length 1nm
 
-sAg = hs.load("../Spectra/PureAgFilip.msa",signal_type="EDS_TEM")
-sCu = hs.load("../Spectra/PureCuFilip.msa",signal_type="EDS_TEM") 
+sAg = hs.load("../Spectra/MCSim 10Cu90Ag.msa",signal_type="EDS_TEM")
+sCu = hs.load("../Spectra/MCSim 90Cu10Ag.msa",signal_type="EDS_TEM") 
 a = CoreShellSpec(x,sCu,sAg)
 particle = a.core + a.shell
 
@@ -33,23 +34,60 @@ p.axes_manager[-1].name = 'E'
 p.add_elements(['Ag','Cu']) #Lägger in ämnen igen tydligen förs de inte med 
 p.add_lines(['Ag_La','Cu_Ka'])
 
-p.get_calibration_from(sAg)
+cal = hs.load("../Spectra/AgCu.msa",signal_type="EDS_TEM")
+p.get_calibration_from(cal)
 p.axes_manager.indices = [25,25] #Sätter "pekaren" på mitten av partikeln, för att få spektrat från den punkten i plotten
 p.plot(True)           
 
 im = p.get_lines_intensity()
+'''
 hs.plot.plot_images(im, tight_layout=True, cmap='RdYlBu_r', axes_decor='off',
     colorbar='single', vmin='1th', vmax='99th', scalebar='all',
     scalebar_color='black', suptitle_fontsize=16,
     padding={'top':0.8, 'bottom':0.10, 'left':0.05,
              'right':0.85, 'wspace':0.20, 'hspace':0.10})
+'''
 kfactors = [2.32, 1.58] # [Ag_La,Cu_Ka] tagna från Boken TransmissionElectronMicroscopy (finns på LUB). Cu är för Ka, Ag La+Lb
 
 
-p.decomposition(algorithm='NMF',output_dimension = 5)
+decomp_dim = 20 #Prova runt med olika antal faktorer, det ger ganska intressanta resultat.
+
+#Var också tvungen att byta datatyp, kör sedan både först NMF och sedan BSS med fastICA
+#p.change_dtype('float64')
+p.decomposition(algorithm='NMF',output_dimension = decomp_dim)
+factors = p.get_decomposition_factors() #Tar ut faktorerna dvs spektrum
+loadings =p.get_decomposition_loadings() #loadings är det återskapade bilderna baserat på faktorerna 
+p.blind_source_separation(number_of_components=decomp_dim)#,algorithm="orthomax"
+bssfac = p.get_bss_factors()
+bssload = p.get_bss_loadings()
+
+# Jag tycker denna är enklare ibland bara för att se vilket spektrum som hör till vilken bild.
+
+p.decomposition(algorithm='NMF',output_dimension = decomp_dim)
 factors = p.get_decomposition_factors()
 hs.plot.plot_spectra(factors,style='cascade')
 p.plot_decomposition_results()
+
+# Men de här är såklart snyggare.
+'''
+plt.title('NMF')
+plt.text(x=8040, y=0.8, s='Cu-K$_\\alpha$', color='k')
+plt.axvline(8040, c='k', ls=':', lw=0.5)
+plt.text(x=930, y=0.8, s='Cu-L$_\\alpha$', color='k')
+plt.axvline(930, c='k', ls=':', lw=0.5)
+plt.axvline(2984, c='k', ls=':', lw=0.5)
+plt.text(x=2984, y=0.8, s='Ag-L$_\\alpha$', color='k')
+plt.axvline(277, c='k', ls=':', lw=0.5)
+plt.text(x=277, y=0.8, s='C-K$_\\alpha$', color='k')
+plt.axvline(1740, c='k', ls=':', lw=0.5)
+plt.text(x=1740, y=0.8, s='Si-K$_\\alpha$', color='k')
+
+hs.plot.plot_images(loadings, cmap='mpl_colors',
+            axes_decor='off', per_row=2,
+            scalebar=[0], scalebar_color='white',
+            padding={'top': 0.95, 'bottom': 0.05,
+                     'left': 0.05, 'right':0.78})
+
 
 #Plottar motvarande fast nu för BSS/fastICA
 hs.plot.plot_spectra(bssfac.isig[0.0:10000.0],style='cascade') 
@@ -71,3 +109,4 @@ hs.plot.plot_images(bssload, cmap='mpl_colors',
             padding={'top': 0.95, 'bottom': 0.05,
                      'left': 0.05, 'right':0.78})
 
+'''
