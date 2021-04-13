37fc07707e0a5adc17ba0832bae73f52a61fcfa0
NanjeofKro <77793340+NanjeofKro@users.noreply.github.com>
2021-04-13 10:36:22 +0200

    Städat modulen
    Skapat en modul core_shell_sim som innehåller alla funktioner vi definierat

---
+++ Arkiv 13042021/edxmat.py
@@ -0,0 +1,38 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Feb  1 16:12:36 2021
+
+@author: Jonas
+
+Method to create matrix with sphere
+"""
+import math;
+import numpy as np;
+class EdxMat:
+    def __init__(self,size,r: float,dens:float,l: float):
+        '''size: defines the 'picture' size as size x size pixels\n
+        r: defines the radius of the spherical particle\n
+        dens: density of the material the particle is made of\n
+        l: pixel size in the same unit as r (i.e. the area is l^2)'''
+        self.size=size;
+        self.r=r;
+        self.dens=dens;
+        self.l=l;
+        rred=r/l;
+        self.mid= float((size-1)/2);#-1 because indices start from zero
+        #The constructor now constructs a matrix using the input arguments
+        mat=np.zeros((size,size));
+        self.mat=mat;
+        for i in range(size):
+            for j in range(size):
+                if rred**2>=((i-self.mid)**2+(j-self.mid)**2):
+                    mat[i][j]=2*dens*self.thick(i,j);
+        
+    def thick(self,n,m):
+        nr=n-self.mid;
+        mr=m-self.mid;
+        l=self.l;
+        r=self.r;
+        return math.sqrt(r**2-(nr)**2-(mr)**2)*l**2; # Skrev om denna för tror inte
+                                                     # den gav thickness i volym.
+    
